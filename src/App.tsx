76f083a53efc84df51ebb2c4bcd564f58244e767
“Ashutosh-Bhadauriya” <ashutoshb455@gmail.com>
2023-10-25 12:37:52 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import './App.css'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import Home from './pages/home';
+import Login from './pages/login';
+import Dashboard from './pages/dashboard';
+
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route index element={<Home />} />
+      <Route path="login" element={<Login />} />
+      <Route path="dashboard" element={<Dashboard />} />
+    </Route>
+  )
+)
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  )
+}
+
+export default App
